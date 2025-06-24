c73a504997761cba052379efab04cc32647c2fa5
timdjol <02_07_92@mail.ru>
2025-06-24 23:53:22 +0600

    repair hotel files

--- resources/views/livewire/hotel-wizard.blade.php
+++ resources/views/livewire/hotel-wizard.blade.php
@@ -208,9 +208,7 @@
                     </div>
 
                 @elseif ($step == 2)
-
                     <div class="row">
-
                         <div class="col-md-6">
                             <div class="form-group">
                                 @error('description')
@@ -879,7 +877,6 @@
         </div>
     </div>
 </div>
-
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
